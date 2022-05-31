48da2d9676b266f4aca9705cf9e15ddbcceae9d4
peaceboyhehe <104025430+peaceboyhehe@users.noreply.github.com>
2022-05-31 15:48:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.APIs = {
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+	'https://zenzapis.xyz': '5466fba554e5', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 
 //customize settings\\
